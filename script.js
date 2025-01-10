469c48949c2f85fc2ba0356ee2ffb0c98abcb051
fikretgozutok <gozutokfikret@gmail.com>
2025-01-11 01:13:10 +0300

    fix mobile

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 let currentPage = 0;
 let previousPage = -1;
 const pages = document.querySelectorAll(".page");
+let startY = 0;
 
-function scrollPage(event) {
+function scrollPageDesktop(event) {
   if (event.deltaY > 0) {
-    // Aşağı kaydırma (next page)
     currentPage = Math.min(currentPage + 1, pages.length - 1);
   } else {
-    // Yukarı kaydırma (previous page)
+    currentPage = Math.max(currentPage - 1, 0);
+  }
+
+  changePage();
+}
+
+function scrollPageMobile(event) {
+  const touch = event.changedTouches[0];
+  const endY = touch.clientY;
+
+  if (startY - endY > 50) {
+    currentPage = Math.min(currentPage + 1, pages.length - 1);
+  } else if (endY - startY > 50) {
     currentPage = Math.max(currentPage - 1, 0);
   }
 
@@ -23,11 +35,18 @@ function changePage() {
   setTimeout(() => {
     pages[currentPage].classList.remove("deactive");
     pages[currentPage].classList.add("active");
-  }, 250);
+  }, 1500);
 
   previousPage = currentPage;
 }
 
-window.addEventListener("wheel", scrollPage);
+if ("ontouchstart" in window) {
+  window.addEventListener("touchstart", (event) => {
+    startY = event.touches[0].clientY;
+  });
+  window.addEventListener("touchend", scrollPageMobile);
+} else {
+  window.addEventListener("wheel", scrollPageDesktop);
+}
 
-changePage(); // İlk sayfa yüklendikten sonra aktif hale getir
+changePage();
